811d66c707c73634832e1fbc538bc423e268a059
AliStanley <98009243+AliStanley@users.noreply.github.com>
2022-01-19 01:20:04 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/********************************************************************************
+* Lab 01
+*********************************************************************************
+* Ali Stanley
+*01/18
+* CMSC255 C90
+********************************************************************************/
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Ali Stanley");
+		System.out.println("Lab 01");
+		System.out.println("This lab is being used to make sure that Java is installed and running propperly.");
+		System.out.println("01/18/22");
+		System.out.println("CMSC255 C90");
+		System.out.println();
+		
+	}
+}
